adf58fc272597214031c689b24508fac5bf7e925
Abarna7837 <abarnagovindhan@gmail.com>
2026-02-07 21:33:05 +0530

    Profile page

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-  
-=======
-  plugins: [react(),tailwindcss(),],
->>>>>>> 7579c292de21203213f16a6f75481be02ccd5bde
+  plugins: [react(), tailwindcss()],
 })
