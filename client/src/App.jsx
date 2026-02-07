f2e2ad2fd248cfcb090c17de48535ff22f0e5bbb
Abarna7837 <abarnagovindhan@gmail.com>
2026-02-08 00:36:14 +0530

    work space added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -6,27 +5,19 @@ import Login from "./pages/Login";
 import Register from "./pages/Signup";
 import ProfilePage from "./pages/ProfilePage";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD
 import Workspace from "./pages/Workspace";
-=======
 import ProtectedRoute from "./config/protectedRoute";
->>>>>>> e9ed910f39d7cb46db3bd687b36ecc516f5e87f2
 
 function App() {
   return (
     <Routes>
       
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/workspace" element={<Workspace />} />
-=======
->>>>>>> e9ed910f39d7cb46db3bd687b36ecc516f5e87f2
+<Route path="/login" element={<Login />} />
+<Route path="/register" element={<Register />} />
+<Route path="/workspace" element={<Workspace />} />
 
-      {/* Protected Routes */}
+{/* Protected Routes */}
       <Route
         path="/dashboard"
         element={
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -108,9 +108,7 @@ function Login() {
               disabled={loading}
               required
             />
-            <span onClick={() => navigate("/register")} style={{ cursor: "pointer", color: "#007bff" }}>
-              Register
-            </span>
+           
             <button type="submit" disabled={loading}>
               {loading ? "Logging in..." : "Login"}
             </button>
