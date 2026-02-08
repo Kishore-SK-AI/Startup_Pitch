d36adbc1d2be73de9a21476d960c9aa86eba9ef2
Kishore-SK-AI <sk2007kishore@gmail.com>
2026-02-08 09:15:48 +0530

    cors updation

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.VITE_API_URL,
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 
